8f487f19a3be5aedda0fa57cb15bf3935a5faa82
YaroslavP32 <yaroslavp32@gmail.com>
2017-05-24 20:13:20 +0300

    Improved 'currentPoint' function

--- main.js
+++ main.js
@@ -21,31 +21,30 @@ class ImprovedFighter extends Fighter {
 }
 
 function Fight(Fighter, ImprovedFighter, ...point) {
+    
     let pointNumber = 0;
-    let currentPoint = point[pointNumber];
-    function changePoint() {
-        ++pointNumber;
-        if (pointNumber >= point.length - 1) pointNumber = 0;
-        currentPoint = point[pointNumber];
+    function currentPoint() {        
+        ++pointNumber;        
+        if (pointNumber > point.length - 1) pointNumber = 0;       
+        return point[pointNumber];
     }
     
     console.log('Ready? Fight!');
     while (true) {
-        Fighter.hit(ImprovedFighter, currentPoint);
+        Fighter.hit(ImprovedFighter, currentPoint());
         if (ImprovedFighter.health <= 0) {
             console.log(`${Fighter.name} makes critical hit and WINS!!!`);
             return false;
         }
-        changePoint();
-        ImprovedFighter.doubleHit(Fighter, currentPoint);
+        ImprovedFighter.doubleHit(Fighter, currentPoint());
         if (Fighter.health <= 0) {
             console.log(`${ImprovedFighter.name} makes critical hit and WINS!!!`);
             return false;
         }
-        changePoint();
+       
     }
 }
 
-var Hulk = new Fighter('Hulk', 1, 60);
-var Batman = new ImprovedFighter('Batman', 1, 35);
-Fight(Hulk, Batman, 1, 2, 3, 4);
+var Hulk = new Fighter('Hulk', 2, 60);
+var Batman = new ImprovedFighter('Batman', 1, 60);
+Fight(Hulk, Batman, 1, 2, 3);
